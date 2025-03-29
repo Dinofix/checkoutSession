0c85a039ee817d1e1979abe8b5511a52ff8b5c1b
Adnan <56862387+Dinofix@users.noreply.github.com>
2025-03-30 00:39:55 +0100

    remove localhost

--- client/src/Confirmation.tsx
+++ client/src/Confirmation.tsx
@@ -19,7 +19,7 @@ const Confirmation = () => {
 
       try {
         const response = await fetch(
-          "http://localhost:3001/payments/verify-session",
+          import.meta.env.VITE_BACKEND_URL + "/payments/verify-session",
           {
             method: "POST",
             headers: {
